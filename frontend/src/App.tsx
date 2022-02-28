9fd627c1caad43a1ba230e4f18636e1bf1de80a7
Augusto Nunes <augustocesar.udi@gmail.com>
2022-02-28 14:32:33 -0300

    Project created and clean, bootstrap, routes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './assets/styles/custom.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!!!</h1>
+    </>
+  );
+}
+
+export default App;
